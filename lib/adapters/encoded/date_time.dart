cc5d3a97ff149a9c4a5dd9777b3d3abac4717567
jozz <yosefd99@gmail.com>
2025-05-17 02:06:12 +0300

    Fixed web error
    DartError: Unsupported operation: Int64 accessor not supported by dart2js.

--- lib/adapters/encoded/date_time.dart
+++ lib/adapters/encoded/date_time.dart
@@ -14,8 +14,12 @@ class DateTimeAdapter extends PrfEncodedAdapter<DateTime, String> {
     if (base64 == null) return null;
     try {
       final bytes = base64Decode(base64);
-      if (bytes.length < 8) return null; // Too short to contain a timestamp
-      final micros = ByteData.sublistView(bytes).getInt64(0, Endian.big);
+      if (bytes.length < 8) return null;
+
+      final data = ByteData.sublistView(bytes);
+      final high = data.getUint32(0, Endian.big);
+      final low = data.getUint32(4, Endian.big);
+      final micros = (high << 32) | low;
       return DateTime.fromMicrosecondsSinceEpoch(micros);
     } catch (_) {
       return null;
@@ -24,8 +28,10 @@ class DateTimeAdapter extends PrfEncodedAdapter<DateTime, String> {
 
   @override
   String encode(DateTime dateTime) {
+    final micros = dateTime.microsecondsSinceEpoch;
     final buffer = ByteData(8);
-    buffer.setInt64(0, dateTime.microsecondsSinceEpoch, Endian.big);
+    buffer.setUint32(0, micros >> 32, Endian.big); // high bits
+    buffer.setUint32(4, micros & 0xFFFFFFFF, Endian.big); // low bits
     return base64Encode(buffer.buffer.asUint8List());
   }
 }
